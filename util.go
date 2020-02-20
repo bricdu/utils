61846e80a5ab22b426b60ba569bf4dfd712da475
du <309587767@qq.com>
2020-02-20 20:37:00 +0800

    add

--- util.go
+++ util.go
@@ -60,3 +60,15 @@ func InOf(in int, arr []int) bool {
 	}
 	return false
 }
+
+// RandStringRunes 返回随机字符串含大写
+func RandStringRunes(n int) string {
+	var letterRunes = []rune("1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+	rand.Seed(time.Now().UnixNano())
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
